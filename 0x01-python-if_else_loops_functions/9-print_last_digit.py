955a616af620984fef9ebf29ff9f4a57bd386e22
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-08 15:03:11 -0700

    9-print

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     x = number
-    if x >= 0:
+    if x < 0:
+        x = x * -1
         x = x % 10
         return x
     else:
-        x = (-1 * x) % 10
+        x = (x) % 10
     return x
     print("{}".format(x), end="")
