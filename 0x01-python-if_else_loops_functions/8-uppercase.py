ae9a6ed54fc2271aeaa9478682f70c5226835dba
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-08 15:15:59 -0700

    8-uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for x in range(len(str)):
-        y = ord(str[x])
-        upper = chr(y - 32)
-        print("{}".format(upper) if y >= 97 and
-                y <= 122 else "{}".format(str[x]), end="")
-    print()
+    output = ""
+    for x in str:
+        if 97 <= x <= 122:
+            output += chr(ord(x) - 32)
+        else:
+            output += x
+    print(output)
--- 0x01-python-if_else_loops_functions/9-main.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-print_last_digit = __import__('9-print_last_digit').print_last_digit
-
-print_last_digit(98)
-r = print_last_digit(0)
-print(r)
