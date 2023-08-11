f7a6233a049b0a15145f426c066c758783526e38
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-11 13:22:48 -0700

    103 print

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("".join(chr(x) for x in range(ord('A'), ord('Z') + 1)))
+from base import done
+done()
---
+++ 0x02-python-import_modules/base.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def done():
+    for x in range(ord('A'), ord('Z') + 1):
+        print(chr(x), end='')
+    print("")
