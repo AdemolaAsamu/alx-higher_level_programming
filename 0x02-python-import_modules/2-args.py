a707885a71c818faa5ba019a47d932b9fc925cd1
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-11 10:48:04 -0700

    ademola

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,11 @@ if __name__ == "__main__":
     var = len(argv)
     if var == 1:
         print("{:d} arguments.".format(var - 1))
-    elif var > 0:
-        for comp in range(1, var):
-            print("{:d}: {}".format(comp, argv[comp]))
+    elif var == 2:
+        print("{:d} argument:".format(var - 1))
+        print("{:d}: {}".format(var - 1, argv[1]))
+    else:
+        print("{:d} arguments:".format(var - 1))
+        for x in range(1, var):
+            print("{:d}: {}".format(var, argv[var]))
 
