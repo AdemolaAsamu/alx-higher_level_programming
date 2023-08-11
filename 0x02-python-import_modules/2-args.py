aa072b6cd2bfd019159c78cfa6f10a2644266a49
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-11 11:29:34 -0700

    2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 from sys import argv
+
 if __name__ != "__main__":
     exit()
 
 var = len(argv)
-
 if var == 1:
     var = 0
-    print("{} arguments:".format(var - 1) if var > 0
-    else "{} arguments.".format(var))
-if var > 0:
-    for com in range(1, val):
-        print("{}: {}".format(com, argv[com]))
+print("{} arguments:".format(var - 1) if var > 0
+        else"{} arguments.".format(var))
+if count > 0:
+    for com in range(1, var):
+        print("{}: {}".format(com, arg[com]))
