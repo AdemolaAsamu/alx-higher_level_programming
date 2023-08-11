31c2b1e473e7b79e9561efc8ceee87933131cb2d
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-11 11:56:42 -0700

    3-infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    from sys import argv
+from sys import argv
+
+def retro(*args):
     sum = 0
-    size = len(argv)
-    from x in range(1, size)):
-        sum += int(argv[x])
-    print("{}".format(sum))
+    size = len(args)
+    if size == 0:
+        print("{}".format(sum))
+    else:
+        for i in range(0, size):
+            sum += int(args[i])
+        print("{}".format(sum))
+
+if __name__ == '__main__':
+    retro(*argv[1:])
