7f8913b7fc2c4c21cd6083b16d8f0d4120767bfb
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-11 12:43:06 -0700

    100-Calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    from calculator_1 import add, sub, mul, div
+    import calculator_1
     size = len(sys.argv)
     if size != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
