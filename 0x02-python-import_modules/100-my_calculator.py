fe3dc2a3fdcd8efc5b2641cad8554c7be853f181
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-11 12:51:22 -0700

    100-calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     import sys
     import calculator_1
     size = len(sys.argv)
-    if size != 4:
+    if size != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     a = int(sys.argv[1])
@@ -11,13 +11,16 @@ if __name__ == '__main__':
     b = int(sys.argv[3])
     if operator == '+':
         out = add(a, b)
+        print(f"{a} {operator} {b} = {out}")
     elif operator == '-':
         out = sub(a, b)
+        print(f"{a} {operator} {b} = {out}")
     elif operator == '*':
         out = mul(a, b)
+        print(f"{a} {operator} {b} = {out}")
     elif operator == '/':
         out = div(a, b)
+        print(f"{a} {operator} {b} = {out}")
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    print(f"{a} {operator} {b} = {out}")
