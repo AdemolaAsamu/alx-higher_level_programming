31e7261853e6d120fb422f19cb2cb384ee2ccf00
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-11 12:46:19 -0700

    100-calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -20,4 +20,4 @@ if __name__ == '__main__':
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    print(f"{a} {operator} {b} = {out})
+    print(f"{a} {operator} {b} = {out}")
