68b8948c4f0b8001024f956cb119ede9f87603df
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-11 11:40:09 -0700

    2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     from sys import argv
-    col = len(argv)
-    if col == 1:
-        col = 0
-    print("{} arguments:".format(col - 1) if col > 0
-            else "{} arguments.".format(col))
-    if col > 0:
-        for i in range(1, col):
+    x = len(argv)
+    if x == 1:
+        x = 0
+    print("{} arguments:".format(x - 1) if x > 0 else "{} arguments.".format(x))
+    if x > 0:
+        for i in range(1, x):
             print("{}: {}".format(i, argv[i]))
