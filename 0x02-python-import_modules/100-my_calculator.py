c4aea1277317f850e0ac6525f89d04dc9de236f2
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-11 12:29:11 -0700

    100-Calculation

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    from calculator_1 import add, sub, mul, div
+    size = len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <symbol> <b>")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    sym = sys.argv[2]
+    b = int(sys.argv[3])
+    if sym == '+':
+        out = add(a, b)
+    elif sym == '-':
+        out = sub(a, b)
+    elif sym == '*':
+        out = mul(a, b)
+    elif sym == '/':
+        out = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    print(f"{a} {sym} {b} = {out})
