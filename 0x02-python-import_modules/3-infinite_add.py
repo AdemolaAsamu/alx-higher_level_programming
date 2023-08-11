79e0a35277a4508ffbce506f961d18928f4839a7
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-11 11:03:41 -0700

    3-infinite_add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+    sum = 0
+    size = len(argv)
+    from x in range(1, size)):
+        sum += int(argv[x])
+    print("{}".format(sum))
