e4e09027b7ceea6aee61a1fdea24a29b138dcaf9
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-14 23:56:28 -0700

    Correctiong Task 5

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = list(my_list)
-    x  = len(my_list) - 1
+    x = len(my_list) - 1
     if idx < 0:
         return new_list
     elif idx > x:
