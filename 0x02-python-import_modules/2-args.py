9ff09b37c0bfab2d269c8b242e03d11d88b19e9f
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-15 00:03:20 -0700

    Correction Task 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     x = len(argv)
     if x == 1:
         x = x - 1
-    print("{} arguments:".format(x - 1) if x > 0 else "{} arguments.".format(x))
+    print("{} argument:".format(x - 1) if x > 0 else "{} arguments.".format(x))
     if x > 0:
         for i in range(1, x):
             print("{}: {}".format(i, argv[i]))
--- 0x02-python-import_modules/README.md
+++ 0x02-python-import_modules/README.md
@@ -1,2 +1,17 @@
  This is the README file for the import function and 
 Create Module Tasks of the ALx Higher LEvel Programming Sprint
+0-add.py
+100-my_calculator.py
+101-easy_print.py
+103-fast_alphabet.py
+1-calculation.py
+2-args.py
+3-infinite_add.py
+4-hidden_discovery.py
+5-variable_load.py
+add_0.py
+base.py
+git
+__pycache__
+README.md
+URM
