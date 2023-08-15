0dc5a96c7cae0562ec5f6dbada0e584d7b72a13f
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-15 00:06:19 -0700

    Task 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,10 @@ if __name__ != '__main__':
 if __name__ == '__main__':
     from sys import argv
     x = len(argv)
+    y = x - 1
     if x == 1:
         x = x - 1
-    print("{} arguments:".format(x - 1) if x > 0 else "{} arguments.".format(x))
+    print("{} arguments:".format(y) if x > 0 else "{} arguments.".format(x))
     if x > 0:
         for i in range(1, x):
             print("{}: {}".format(i, argv[i]))
