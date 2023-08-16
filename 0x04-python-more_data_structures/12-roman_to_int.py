49efe0f593c15ff8b6e0c7a97f6789eba44dffa9
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-16 15:29:32 -0700

    Task 13

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    intval = 0
+    vault = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+    test = isinstance(roman_string, str)
+    if roman_string is None or test != True:
+        return intval
+    x = len(roman_string) - 1
+    for y in range(x):
+        if vault.get(roman_string[y]) < vault.get(roman_string[y + 1]):
+            intval -= vault.get(roman_string[y])
+        else:
+            intval += vault.get(roman_string[y])
+    intval += vault.get(roman_string[x]) 
+    return intval
