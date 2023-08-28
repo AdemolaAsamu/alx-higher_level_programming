09c7b556b126a674207fc703fa05a6ef0b8c364c
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-28 12:40:00 -0700

    Task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    division = None
+    try:
+        division = a / b
+    except ZeroDivisionError:
+        pass
+    finally:
+        print("Inside result: {}".format(division))
+        return division
