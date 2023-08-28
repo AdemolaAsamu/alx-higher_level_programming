29aa016f3bb98f990992e29d24fd099552f937d5
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-28 12:54:51 -0700

    Update Task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,7 +7,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             division = my_list_1[num] / my_list_2[num]
         except IndexError:
-            print("Out of range")
+            print("out of range")
             division = 0
         except ZeroDivisionError:
             print("division by 0")
