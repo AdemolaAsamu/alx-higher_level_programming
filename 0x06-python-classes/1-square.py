40c3446178bd3fea37584202d6ccbeb7f0d7fd10
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-29 11:52:58 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is a module that handles square class
+"""
+
+class Square:
+    """
+    This is the class for the square
+    """
+    def __init__(self, size=0):
+        self.__size = size
