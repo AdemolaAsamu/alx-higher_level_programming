9238d2448bb9dc5022ba24a85a12075ade5c1082
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-08-29 12:04:52 -0700

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,3 +5,15 @@ lass Square that defines a square by
 """
 
 
+class Square:
+    """
+    This is the square class with type error
+    and value error handlers
+    """
+    def __init__(self, size=0):
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        elif type(size) != int:
+            raise TypeError("size must be an integer")
+        else:
+            self.__size = size
