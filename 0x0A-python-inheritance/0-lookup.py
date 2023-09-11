e82fe6b7bd74b8e2177e77149dfdb0dab2b0ebe1
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-09-11 12:06:16 -0700

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module executes the class attributes
+"""
+
+
+def lookup(obj):
+    """
+    This function class returns a list of
+    Object attributes
+    """
+
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,8 @@
+This is the README File for the 
+
+## 0x0A-python-inheritance Tasks
+
+#Author : Ademola ASAMU
+
+### List of Contents
+
