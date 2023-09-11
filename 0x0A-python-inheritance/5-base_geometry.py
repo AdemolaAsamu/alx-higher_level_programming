ba0a95a4adb0d35854c80387429a68aa45edaf7a
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-09-11 13:01:25 -0700

    Task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is a module that contains
+an empty class base grometry
+"""
+
+
+class BaseGeometry:
+    """
+    This is an empty class
+    BaseGgeometry
+    """
+    pass
