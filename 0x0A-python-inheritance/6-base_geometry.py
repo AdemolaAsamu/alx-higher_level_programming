7dd1a7035ea6fa9637d886a96706630d05317edc
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-09-11 13:11:10 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This is a module that contains
+a class base grometry
+Builds on Task 5
+"""
+
+
+class BaseGeometry:
+    """
+    This is a Class BaseGgeometry
+    """
+    
+    def area(self):
+        """
+        Public instance method area(self)
+        """
+        raise Exception("area() is not implemented")
