5705e6309ee974428f0f962f980c4bc00b33ca90
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-09-11 12:51:46 -0700

    Task 4 : Sub class inheritance check

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module that handles a class
+inheritance status
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    This checks if it inherited from a subclass
+    """
+
+    if type(obj) != a_class:
+        return False
+    if issubclass(type(obj), a_class):
+        return True
+    return False
