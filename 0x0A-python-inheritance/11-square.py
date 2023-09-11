fbbea8d7a9af8e6e7773d6ac6fb320d06c346d25
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-09-11 14:50:37 -0700

    Task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module executes Class Square which inherits
+from Task 9 Rectangle
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class Square that inherits from Rectangle
+    """
+
+    def __init__(self, size):
+        """
+        Initialises with size
+        """
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+        super().__init__(size, size)
