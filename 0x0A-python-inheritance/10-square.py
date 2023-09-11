b2d2825e086c4561ce743cfa909bdadeb4ad02c3
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-09-11 14:41:45 -0700

    Task 10 Update

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -16,6 +16,6 @@ class Square(Rectangle):
         Initialises with size
         """
 
-        self.integer_validator("size", size)
         super().__init__(size, size)
+
         self.__size = size
