3c9cc4be5739493770a7edbee71f2a7327b07e0a
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-09-12 12:23:26 -0700

    Task 7: Corrected for Indentation Error

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
 def add_and_save_to_list():
-   """"
+   """
    This is the function that adds and saces
    """
    try:
@@ -18,7 +18,7 @@ def add_and_save_to_list():
         for arg in range(1, size):
             load_data.append(sys.argv[arg])
         save_to_json_file(load_data, "add_item.json")
-    except FileNotFoundError:
+   except FileNotFoundError:
         with open("add_item.json", "w") as content:
             content.write("[]")
         load_data = load_from_json_file("add_item.json")
