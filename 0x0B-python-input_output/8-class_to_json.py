4107bf7a9da7fa3f09b1da02fe2df76417ee743a
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-09-12 12:31:45 -0700

    Task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Function that class to json
+"""
+
+def class_to_json(obj):
+    """
+    Function that returns the dictionary
+    of an object
+    """
+    return obj.__dict__
