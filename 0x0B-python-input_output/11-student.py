fbaa5fc0d04c6f6ec4abd2ba00d590c641e8b7c5
Ademola ASAMU <Ademola.Asamu@gmail.com>
2023-09-12 12:57:49 -0700

    Task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Class - Student
+"""
+
+
+class Student:
+    """
+    This is a student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initilising the instances
+        based on the variables given
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns the TOC
+        """
+        toc = self.__dict__
+        if attrs is None:
+            return toc
+        elif isinstance(attrs, list):
+            new_toc = {}
+            for key, value in toc.items():
+                if key in attrs:
+                    new_toc[key] = value
+            return new_toc
+
+    def reload_from_json(self, json):
+        """
+        this loads data
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
