7611e4777f6d5aa42371734123ae2783ddd114f8
Ademola ASAMU <Ademola.Asamu@gmail.com>
2024-05-22 14:34:34 -0700

    REfactor Task 3

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,3 @@
 -- Create Table force_name if not exist
 -- table property int varcghar256
-CREATE TABLE IF NOT EXISTS force_name (id INT,
-	name VARCHAR(256) NOT NULL
-	);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
