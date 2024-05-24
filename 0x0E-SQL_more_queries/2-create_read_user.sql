9fbe9fefce63b88b566fa57cdeed8d2b6d3f8c55
Ademola ASAMU <Ademola.Asamu@gmail.com>
2024-05-23 18:53:16 -0700

    update to errors on syntax

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- Create the database hbtn_0d_2 and user user_0d_2
 -- user_0d_2 should have password user_od_2_pwd
 -- if either data exists not to fail
-CREATE  DATEBASE IF NOT EXISTS hbtn_0d_2;
+CREATE  DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
